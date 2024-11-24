90a55ad9ca10a42caebfb9423a443d7116d45eb4
TheMidnightManiac <inscientebberman@tutanota.com>
2024-11-24 14:23:05 -0300

    Update actions

--- actions/functions.sh
+++ actions/functions.sh
@@ -1,15 +1,32 @@
+#!/bin/bash
 # Workaround for using env variables in curl's -d opt
 generate_post_data()
 {
+  # All commits for the given tag
+  COMMITS="$(printf "\n\n%s" "**Commits**: https://github.com/themidnightmaniac/magisk-gcp/commits/${VERSION}")"
   cat <<EOF
 {
 "tag_name":"$VERSION",
 "target_commitish":"master",
-"name":"${VERSION} - ${DATE}",
+"name":"${VERSION} - $(date +'%Y-%m-%d')",
 "body":"$(printf "%q" "${RELEASE}${COMMITS}" | sed "s/'//g;s/\$//g" | tr -d '$')",
 "draft":false,
 "prerelease":false,
 "generate_release_notes":false
 }
 EOF
+}
+cleanup()
+{
+  log "Cleaning up...";
+  for item in "$@"; do
+    if [ -f "$item" ]; then rm -rf "$item"; log "Removed $item!";
+    else log "$item Not found!"; fi
+  done;
+  log "Done!";
+  exit 0;
+}
+log()
+{
+  printf "%s - %s\n" "$(date +"%H:%M:%S")" "$1";
 }
--- actions/release.sh
+++ actions/release.sh
@@ -1,60 +1,71 @@
 #!/bin/bash
 
-. ./env.env
-. ./actions/functions.sh
+#|==============================================================|
+#| Generic GitHub releases tool for magisk modules              |
+#| you can change the variables below for your specific usecase |
+#| will work out of the box in most cases					    |
+#| still needs some tweaking tho							    |
+#| NEEDS GITHUB_TOKEN ENV VAR SET WITH ENOUGH PERMISSIONS	    |
+#|==============================================================|
 
-mkdir temp
+# shellcheck source=./env.env
+source ./env.env
+
+# shellcheck source=./actions/functions.sh
+source ./actions/functions.sh
 
 # Define the required variables and some workarounds
 VERSION="$(cat version)"
-FILE="temp/magisk-gcp-${VERSION}.zip"
-FILENAME="$(basename $FILE)"
+FILE="magisk-gcp-${VERSION}.zip"
 RELEASE="$(cat RELEASE.MD)"
-DATE="$(date +'%Y-%m-%d')"
-COMMITS="$(printf "\n\n%s" "**Commits**: https://github.com/themidnightmaniac/magisk-gcp/commits/${VERSION}")"
+REPO="themidnightmaniac/test"
 
-# Exit if tag already exists locally
-if [ "$(git tag "$VERSION")" ]; then printf "Tag Already Exists! Exiting...\n" && exit 1;
+trap "cleanup "$FILE"" SIGINT SIGTERM EXIT
+
+if [ -f "$FILE" ]; then log "$FILE Already exists! Exiting..." && exit 1; fi
+
+# Create tag for the new release and exit if tag already exists locally
+if ! git tag "$VERSION" 2> /dev/null; then
+  log "Tag Already Exists! Exiting..." && exit 1;
 else
-  printf "Pushing new tag...\n";
-	for remote in $(git remote); do git push "$remote" master; done;
+  log "Pushing new tag...";
+  for remote in $(git remote); do git push "$remote" master 2> /dev/null; done;
 fi
 
 # Add release text to the changelog
-printf "Adding release info to changelog...\n"
+log "Adding release info to changelog..."
 CHANGELOG="$(printf "%s\n\n" "$RELEASE" && tail -n +3 CHANGELOG.MD)"
 echo "$CHANGELOG" > CHANGELOG.MD
 
 # Zip the release
-printf "Creating zip file...\n"
-zip -r9 "$FILE" ./* -x "*.zip" -x "*.env" -x "actions/*" -x "*.MD" -x ".gitattributes" -x ".gitignore" -x ".git/*" -x "LICENSE" -x "temp" 1>/dev/null
+log "Creating zip file..."
+zip -r9 "$FILE" ./* -x "*.zip" -x "*.env" -x "actions/*" -x "*.MD" -x ".gitattributes" -x ".gitignore" -x ".git/*" -x "LICENSE" -x "temp/*" 1>/dev/null
 
 # Create the release and capture the response into a variable
+log "Creating release on GitHub..."
 RESPONSE1="$(curl -L -s \
   -X POST \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer $GITHUB_TOKEN" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  "https://api.github.com/repos/themidnightmaniac/magisk-gcp/releases" \
+  "https://api.github.com/repos/$REPO/releases" \
   --data "$(generate_post_data)")"
 
 # Extract the RELEASE_ID from the JSON response using jq
 RELEASE_ID=$(echo "$RESPONSE1" | jq -r '.id')
 
 # Throw an error if no .id is found in the response
-if [ -z "$RELEASE_ID" ]; then printf "Failed to retrieve release ID. Exiting...\n"; exit 1; fi;
+if [ -z "$RELEASE_ID" ]; then log "Failed to retrieve release ID. Exiting..."; exit 1; fi;
 
-# TODO: Check if uploading the release zip was a success
-RESPONSE2="$(curl -L \
+# Upload asset to GitHub
+log "Uploading $FILE to GitHub..."
+RESPONSE2="$(curl -L -s \
+  -w "%{http_code}" \
   -X POST \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer $GITHUB_TOKEN" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   -H "Content-Type: application/octet-stream" \
-  "https://uploads.github.com/repos/themidnightmaniac/magisk-gcp/releases/$RELEASE_ID/assets?name=$FILENAME" \
+  "https://uploads.github.com/repos/$REPO/releases/$RELEASE_ID/assets?name=$FILE" \
   --data-binary "@${FILE}")"
-
-# Clean up
-printf "Cleaning up...\n"
-rm -rf "temp"
 
